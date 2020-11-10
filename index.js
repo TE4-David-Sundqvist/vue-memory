cfc2f3b17a9affabcef809d77aa1c0fbed18fd5e
David Sundqvist <david.sundqvist@elev.ga.ntig.se>
2020-11-10 14:28:04 +0100

    we did more stuff

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ const App = {
     },
     methods: {
 
+        start(){
+
+            list_of_imgs = ['1_pig', '2_squirrel', '3_rabbit', '4_frog', '5_fox', '6_bear', '7_monkey', '8_panda', '9_chick', '10_tiger', '11_penguin', '12_racoon', '1_pig', '2_squirrel', '3_rabbit', '4_frog', '5_fox', '6_bear', '7_monkey', '8_panda', '9_chick', '10_tiger', '11_penguin', '12_racoon']
+            
+            for(img of list_of_imgs){
+                this.cards.push({
+                    img: "img/" + img + ".png",
+                    id: list_of_imgs.indexOf(img)
+                });
+            };
+
+        },
     }
 };
 
@@ -36,6 +48,6 @@ app.component('Card', {
         },
     },
     template: '#cardtemplate'
-});
+})
 
 app.mount("#main");
